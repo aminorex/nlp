7b1383a659d13769f9b239c8b8072c4c4fac6b55
aminorex <agit@southoftheclouds.net>
2015-07-17 16:06:57 -0500

    twitter stuff

--- utils/gtrans.py
+++ utils/gtrans.py
@@ -2,14 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import argparse
 import urllib
 import urllib2
 import codecs
+import re
+from HTMLParser import HTMLParser
 
-def translate(text, to_language="auto", from_language="auto", tag=''):
+def translate(text, twitter=False, to_language="auto", from_language="auto", tag=''):
     '''Return the translation using google translate'''
     agents = {'User-Agent':"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"}
     before_trans = 'class="t0">'
+    if twitter: # code for handling username/hashtag capture
+        username = re.compile(r'(@ _ .+?\b|@ .+? _\b|@ \S+?\b) ')
+        hashtag = re.compile(r'(# \S+?\b)', re.U)
+        usrs = re.findall(username, text)
+        tags = re.findall(hashtag, text)
+        if len(usrs) > 0:
+            for i in range(0, len(usrs)-1):
+                sys.stderr.write('Username ' + str(i) + ': ' + usrs[i] + '\n')
+                text = text.replace(usrs[i], '@'+str(i))
+            sys.stderr.write(text + '\n')
+        else:
+            sys.stderr.write('No usernames found on line: ' + text + '\n')
+        if len(tags) > 0:
+            for i in range(0, len(tags)-1):
+                sys.stderr.write('Hashtag ' + str(i) + ': ' + tags[i] + '\n')
+#                text = text.replace(tags[i], '#'+str(i))
+            sys.stderr.write(text + '\n')
+
+    line = text.decode('utf-8').strip()
+    line = map(lambda x: '_' if ord(x) > 0xfff0 else x, line)
+    text = u''.join(line)
     link = "http://translate.google.com/m?hl=%s&sl=%s&q=%s" % (to_language, from_language, urllib.quote_plus(text.encode('utf-8')))
     if tag:
         sys.stderr.write(tag+':: '+link+"\n")
@@ -17,6 +41,12 @@ def translate(text, to_language="auto", from_language="auto", tag=''):
     page = urllib2.urlopen(request).read()
     result = page[page.find(before_trans)+len(before_trans):]
     result = result.split("<")[0]
+    if twitter and len(usrs) > 0:
+        for i in range(0, len(usrs)-1):
+            result.replace('@'+str(i), usrs[i].replace(' ', ''))
+    if twitter and len(tags) > 0:
+        for i in range(0, len(usrs)-1):
+            result.replace('#'+str(i), tags[i].replace(' ', ''))
     return result
 
 if __name__ == '__main__':
@@ -26,65 +56,47 @@ if __name__ == '__main__':
     prog = sys.argv[0]
     start=0
     end=0
-    iarg = 1
+    twitter = False # twitter behavior control
 
-    # parse flags and line ranges
-    while iarg < len(sys.argv):
-        if sys.argv[iarg][0] == '-':
-            if sys.argv[iarg][1] == 'v':
-                verbosity += 1
-            elif str.isdigit(sys.argv[iarg][1:]):
-                if start:
-                    end=int(sys.argv[iarg][1:])
-                else:
-                    start=int(sys.argv[iarg][1:])
-        elif str.isdigit(sys.argv[iarg]):
-            if start:
-                end=int(sys.argv[iarg])
-            else:
-                start=int(sys.argv[iarg])
-        else:
-            break
-        iarg += 1
-
-    if len(sys.argv) < iarg + 2:
-        sys.stderr.write(prog+': Require I1 and I2 input and output iso639-3 codes' + "\n");
+    parser = argparse.ArgumentParser(description="Will run all (or specified) lines of stdin through Google Translate with source and target languages specified, then output the result to stdout.")
+    parser.add_argument('-v', '--verbosity', action='count', help='Increases verbosity of output.')
+    parser.add_argument('-t', '--twitter', action='store_true', help='Enables Twitter-specific behavior.')
+    parser.add_argument('-i', '--infile', help='Source file path.')
+    parser.add_argument('-o', '--outfile', help='Target file path.')
+    parser.add_argument('-s', '--start', type=int, help='Starting target (line number).')
+    parser.add_argument('-e', '--end', type=int, help='Ending target (line number).')
+    parser.add_argument('lin', help='Input language (ISO639-3 code)')
+    parser.add_argument('lout', help='Output language (ISO639-3 code)')
+    args = parser.parse_args()
+    
+    if args.start and not args.end:
+        sys.stderr.write(prog+': Start line given without end line.\n')
         sys.exit(1)
-
-    # input language
-    if len(sys.argv[iarg]) == 2:
-        lin = sys.argv[iarg]
-        iarg += 1
-    else:
-        sys.stderr.write(prog+': Invalid input language '+sys.argv[iarg]+"\n");
+    if args.end and not args.start:
+        sys.stderr.write(prog+': End line given without start line.\n')
         sys.exit(1)
+    if args.start or args.end:
+        start = args.start
+        end = args.end
 
-    # output language
-    if len(sys.argv[iarg]) == 2 and iarg < len(sys.argv):
-        lout = sys.argv[iarg]
-        iarg += 1
-    else:
-        sys.stderr.write(prog+': Invalid output language '+sys.argv[iarg]+"\n");
-        sys.exit(1)
+    if args.twitter:
+        twitter = True
 
-    # input,output files
-    nfiles = len(sys.argv) - iarg
-    if nfiles > 2:
-        sys.stderr.write(prog,': only two paths allowed, input and output'+"\n")
-        sys.exit(1)
-    if nfiles > 0:
-        fin = codecs.open(sys.argv[iarg],encoding='utf-8')
-        iarg += 1
+    if args.infile:
+        fin = codecs.open(args.infile,encoding='utf-8')
     else:
         fin = sys.stdin
-    if nfiles > 1:
-        fout = codecs.open(sys.argv[iarg],'w',encoding='utf-8')
-        iarg += 1
+    if args.outfile:
+        fout = codecs.open(args.outfile,'w',encoding='utf-8')
     else:
         fout = sys.stdout
 
-    if verbosity:
+    if args.verbosity:
+        verbosity = args.verbosity
         sys.stderr.write(prog+': from '+lin+' to '+lout+"\n")
+
+    lin = args.lin
+    lout = args.lout
 
     # translation loop
     nlines = 0
@@ -96,7 +108,15 @@ if __name__ == '__main__':
                 continue
             elif end and end < nlines:
                 break
-            text = translate(line,to_language=lout,from_language=lin,tag=(repr(nlines) if verbosity > 1 else ''))
+            if twitter:
+                h = HTMLParser()
+                line = line.decode('utf-8')
+                line = h.unescape(line)
+                line = line.encode('utf-8')
+                line = line.replace('@-@','-')
+                text = translate(line,twitter=True,to_language=lout,from_language=lin,tag=(repr(nlines) if verbosity > 1 else ''))
+            else:
+                text = translate(line,to_language=lout,from_language=lin,tag=(repr(nlines) if verbosity > 1 else ''))
             fout.write(text+"\n")
         except:
             ty,ob,tr = sys.exc_info()
